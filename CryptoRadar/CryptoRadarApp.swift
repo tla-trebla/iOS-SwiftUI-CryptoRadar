788dc6318019f62af115dc8b48147dddee6870b9
Albert Pangestu <stamina.left@gmail.com>
2024-08-10 16:18:21 +0700

    Initial Commit

---
+++ CryptoRadar/CryptoRadarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoRadarApp.swift
+//  CryptoRadar
+//
+//  Created by Albert Pangestu on 10/08/24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoRadarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
