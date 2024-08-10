a01646cfda0f34eea899dfa24cf2500975732739
Albert Pangestu <stamina.left@gmail.com>
2024-08-10 19:13:44 +0700

    Created test case when `Repository` receive an `Error`, `UseCase` should receive the `Error` too

--- Domain/Tests/DomainTests/UseCase/LoadCryptosUseCaseTest.swift
+++ Domain/Tests/DomainTests/UseCase/LoadCryptosUseCaseTest.swift
@@ -14,13 +14,13 @@ final class LoadCryptosUseCase {
         self.repository = repository
     }
     
-    func load() {
-        repository.load()
+    func load() async throws -> String {
+        try await repository.load()
     }
 }
 
 protocol LoadCryptosRepository {
-    func load()
+    func load() async throws -> String
 }
 
 final class LoadCryptosUseCaseTest: XCTestCase {
@@ -31,21 +31,37 @@ final class LoadCryptosUseCaseTest: XCTestCase {
         XCTAssertEqual(repository.messages, [])
     }
     
-    func test_load_shouldRequest() {
+    func test_load_shouldRequest() async {
         let (sut, repository) = makeSUT()
         
-        sut.load()
+        _ = try? await sut.load()
         
         XCTAssertEqual(repository.messages, [.loaded])
     }
     
-    func test_loadMore_requestMore() {
+    func test_loadMore_requestMore() async {
         let (sut, repository) = makeSUT()
         
-        sut.load()
-        sut.load()
+        _ = try? await sut.load()
+        _ = try? await sut.load()
         
         XCTAssertEqual(repository.messages, [.loaded, .loaded])
+    }
+    
+    func test_load_returnsError() async {
+        let error = NSError(domain: "Any", code: 0)
+        let repository = LoadCryptosRepositoryStub(result: .failure(error))
+        let sut = LoadCryptosUseCase(repository: repository)
+        var capturedError: Error?
+        
+        do {
+            _ = try await sut.load()
+            XCTFail("Expected an error, not a success")
+        } catch {
+            capturedError = error
+        }
+        
+        XCTAssertNotNil(capturedError)
     }
     
     // MARK: - Helpers
@@ -56,11 +72,24 @@ final class LoadCryptosUseCaseTest: XCTestCase {
         return (sut, repository)
     }
     
+    final class LoadCryptosRepositoryStub: LoadCryptosRepository {
+        let result: Result<String, Error>
+        
+        init(result: Result<String, Error>) {
+            self.result = result
+        }
+        
+        func load() async throws -> String {
+            try result.get()
+        }
+    }
+    
     final class LoadCryptosRepositorySpy: LoadCryptosRepository {
         private(set) var messages: [Message] = []
         
-        func load() {
+        func load() async throws -> String {
             messages.append(.loaded)
+            return ""
         }
         
         enum Message {
