99e8b17302363da7d7f43bd603a961c8738d8a5d
Albert Pangestu <stamina.left@gmail.com>
2024-08-12 01:58:54 +0700

    Refactored the `subscribe` function on both `UseCase` and `Repository` by implementing the `AsyncThrowingStream`, and created test case, when `Repository` sends an `Error`, the `UseCase` should receive it too

--- Domain/Tests/DomainTests/UseCase/SubscribeCryptoUpdatesUseCaseTest.swift
+++ Domain/Tests/DomainTests/UseCase/SubscribeCryptoUpdatesUseCaseTest.swift
@@ -15,13 +15,15 @@ final class SubscribeCryptoUpdatesUseCase {
         self.repository = repository
     }
     
-    func subscribe(to cryptos: [String]) async throws -> [CryptoModel] {
-        try await repository.subscribe(to: cryptos)
+    func subscribe(to cryptos: [String]) -> AsyncThrowingStream<[CryptoModel], Error> {
+        let stream = repository.subscribe(to: cryptos)
+        
+        return stream
     }
 }
 
 protocol SubscribeCryptoUpdatesRepository {
-    func subscribe(to cryptos: [String]) async throws -> [CryptoModel]
+    func subscribe(to cryptos: [String]) -> AsyncThrowingStream<[CryptoModel], Error>
 }
 
 final class SubscribeCryptoUpdatesUseCaseTest: XCTestCase {
@@ -32,32 +34,71 @@ final class SubscribeCryptoUpdatesUseCaseTest: XCTestCase {
         XCTAssertEqual(repository.messages, [])
     }
     
-    func test_subscribeOnce_requestOnce() async {
+    func test_subscribeOnce_requestOnce() {
         let repository = SubscribeCryptoUpdatesRepositorySpy()
         let sut = SubscribeCryptoUpdatesUseCase(repository: repository)
         
-        _ = try? await sut.subscribe(to: ["BTC", "ETH"])
+        _ = sut.subscribe(to: ["BTC", "ETH"])
         
         XCTAssertEqual(repository.messages, [.subscribe])
     }
     
-    func test_subscribeMore_requestMore() async {
+    func test_subscribeMore_requestMore() {
         let repository = SubscribeCryptoUpdatesRepositorySpy()
         let sut = SubscribeCryptoUpdatesUseCase(repository: repository)
         
-        _ = try? await sut.subscribe(to: ["BTC", "ETH"])
-        _ = try? await sut.subscribe(to: ["BTC", "ETH"])
+        _ = sut.subscribe(to: ["BTC", "ETH"])
+        _ = sut.subscribe(to: ["BTC", "ETH"])
         
         XCTAssertEqual(repository.messages, [.subscribe, .subscribe])
     }
     
+    func test_subscribe_getsAnError() async {
+        let error = NSError(domain: "Any", code: 0)
+        let repository = SubscribeCryptoUpdatesRepositoryStub(result: .failure(error))
+        let sut = SubscribeCryptoUpdatesUseCase(repository: repository)
+        var capturedError: Error?
+        
+        let stream = sut.subscribe(to: ["BTC", "ETH"])
+        
+        do {
+            for try await _ in stream {
+                XCTFail("Gets a success, not an error")
+            }
+        } catch {
+            capturedError = error
+        }
+        
+        XCTAssertNotNil(capturedError)
+    }
+    
     // MARK: - Helper
+    final class SubscribeCryptoUpdatesRepositoryStub: SubscribeCryptoUpdatesRepository {
+        let result: Result<[CryptoModel], Error>
+        
+        init(result: Result<[CryptoModel], Error>) {
+            self.result = result
+        }
+        
+        func subscribe(to cryptos: [String]) -> AsyncThrowingStream<[CryptoModel], any Error> {
+            AsyncThrowingStream { continuation in
+                switch result {
+                case .success(let models):
+                    continuation.yield(models)
+                    continuation.finish()
+                case .failure(let failure):
+                    continuation.finish(throwing: failure)
+                }
+            }
+        }
+    }
+    
     final class SubscribeCryptoUpdatesRepositorySpy: SubscribeCryptoUpdatesRepository {
         private(set) var messages: [Message] = []
         
-        func subscribe(to cryptos: [String]) async throws -> [CryptoModel] {
+        func subscribe(to cryptos: [String]) -> AsyncThrowingStream<[CryptoModel], Error> {
             messages.append(.subscribe)
-            return [CryptoModel]()
+            return AsyncThrowingStream { _ in }
         }
         
         enum Message {
