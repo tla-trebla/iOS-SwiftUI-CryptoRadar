36a3c521c4ee8dcd39d677b791d884ffaa6ff3ef
Albert Pangestu <stamina.left@gmail.com>
2024-08-11 13:10:36 +0700

    Created test file named `RemoteLoadCryptosRepositoryTest`, and created test case when `Repository` is initialized, it should not request to the `Client`

---
+++ Domain/Tests/DomainTests/Repository/RemoteLoadCryptosRepositoryTest.swift
@@ -0,0 +1,46 @@
+//
+//  RemoteLoadCryptosRepositoryTest.swift
+//  
+//
+//  Created by Albert Pangestu on 11/08/24.
+//
+
+import XCTest
+@testable import Domain
+
+final class RemoteLoadCryptosRepository: LoadCryptosRepository {
+    let client: HTTPClient
+    
+    init(client: HTTPClient) {
+        self.client = client
+    }
+    
+    func load() async throws -> [CryptoModel] {
+        try await client.load()
+    }
+}
+
+protocol HTTPClient {
+    func load() async throws -> [CryptoModel]
+}
+
+final class RemoteLoadCryptosRepositoryTest: XCTestCase {
+
+    func test_initialize_notRequesting() {
+        let client = HTTPClientSpy()
+        let sut = RemoteLoadCryptosRepository(client: client)
+        
+        XCTAssertEqual(client.requestCount, 0)
+    }
+    
+    // MARK: - Helpers
+    final class HTTPClientSpy: HTTPClient {
+        private(set) var requestCount: Int = 0
+        
+        func load() async throws -> [CryptoModel] {
+            requestCount = 1
+            return [CryptoModel]()
+        }
+    }
+    
+}
