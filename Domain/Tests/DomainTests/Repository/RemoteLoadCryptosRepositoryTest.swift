d0a89ce8967959699d90addeb7161c9f69a78eef
Albert Pangestu <stamina.left@gmail.com>
2024-08-11 13:16:40 +0700

    Created test case, when `Repository` request more, the `Client` should request more too

--- Domain/Tests/DomainTests/Repository/RemoteLoadCryptosRepositoryTest.swift
+++ Domain/Tests/DomainTests/Repository/RemoteLoadCryptosRepositoryTest.swift
@@ -42,12 +42,22 @@ final class RemoteLoadCryptosRepositoryTest: XCTestCase {
         XCTAssertEqual(client.requestCount, 1)
     }
     
+    func test_loadMore_requestMore() async {
+        let client = HTTPClientSpy()
+        let sut = RemoteLoadCryptosRepository(client: client)
+        
+        _ = try? await sut.load()
+        _ = try? await sut.load()
+        
+        XCTAssertEqual(client.requestCount, 2)
+    }
+    
     // MARK: - Helpers
     final class HTTPClientSpy: HTTPClient {
         private(set) var requestCount: Int = 0
         
         func load() async throws -> [CryptoModel] {
-            requestCount = 1
+            requestCount += 1
             return [CryptoModel]()
         }
     }
