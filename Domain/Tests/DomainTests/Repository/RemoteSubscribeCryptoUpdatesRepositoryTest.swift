38d589b9993f71bdbe71070bac67a8ce9b79e41a
Albert Pangestu <stamina.left@gmail.com>
2024-08-13 14:43:54 +0700

    Created test case, when `Repository` is streaming more than once, the `Client` should stream more than once too, and refactored the `Client`'s function by implementing the `AsyncThrowingStream`

--- Domain/Tests/DomainTests/Repository/RemoteSubscribeCryptoUpdatesRepositoryTest.swift
+++ Domain/Tests/DomainTests/Repository/RemoteSubscribeCryptoUpdatesRepositoryTest.swift
@@ -16,20 +16,17 @@ final class RemoteSubscribeCryptoUpdatesRepository: SubscribeCryptoUpdatesReposi
     }
     
     func subscribe(to cryptos: [String]) -> AsyncThrowingStream<[CryptoModel], any Error> {
-        AsyncThrowingStream { continuation in
-            Task {
-                do {
-                    _ = try await client.subscribe(to: cryptos)
-                } catch {
-                    continuation.finish(throwing: error)
-                }
-            }
-        }
+        let stream = client.subscribe(to: cryptos)
+        return translateData(from: stream)
+    }
+    
+    private func translateData(from stream: AsyncThrowingStream<(Data, URLResponse), Error>) -> AsyncThrowingStream<[CryptoModel], Error> {
+        AsyncThrowingStream { _ in }
     }
 }
 
 protocol SubscribeCryptoUpdatesHTTPClient {
-    func subscribe(to cryptos: [String]) async throws -> (Data, URLResponse)
+    func subscribe(to cryptos: [String]) -> AsyncThrowingStream<(Data, URLResponse), Error>
 }
 
 final class RemoteSubscribeCryptoUpdatesRepositoryTest: XCTestCase {
@@ -50,13 +47,23 @@ final class RemoteSubscribeCryptoUpdatesRepositoryTest: XCTestCase {
         XCTAssertEqual(client.requestCount, 1)
     }
     
+    func test_subscribeMore_requestMore() {
+        let client = SubscribeCryptoUpdatesHTTPClientSpy()
+        let sut = RemoteSubscribeCryptoUpdatesRepository(client: client)
+        
+        _ = sut.subscribe(to: ["BTC", "ETH"])
+        _ = sut.subscribe(to: ["BTC", "ETH"])
+        
+        XCTAssertEqual(client.requestCount, 2)
+    }
+    
     // MARK: - Helper
     final class SubscribeCryptoUpdatesHTTPClientSpy: SubscribeCryptoUpdatesHTTPClient {
         private(set) var requestCount: Int = 0
         
-        func subscribe(to cryptos: [String]) async throws -> (Data, URLResponse) {
-            requestCount = 1
-            return (Data(), URLResponse())
+        func subscribe(to cryptos: [String]) -> AsyncThrowingStream<(Data, URLResponse), Error> {
+            requestCount += 1
+            return AsyncThrowingStream { _ in }
         }
     }
 }
