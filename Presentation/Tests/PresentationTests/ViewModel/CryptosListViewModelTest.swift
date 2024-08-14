0d05fb2430b10eee57bc99dc61e58e0005e14564
Albert Pangestu <stamina.left@gmail.com>
2024-08-14 23:00:20 +0700

    Created test case, when the `ViewModel` is initialized, the `SubscribeUseCase` should not listen to any updates

--- Presentation/Tests/PresentationTests/ViewModel/CryptosListViewModelTest.swift
+++ Presentation/Tests/PresentationTests/ViewModel/CryptosListViewModelTest.swift
@@ -10,9 +10,11 @@ import Domain
 
 final class CryptosListViewModel: ObservableObject {
     let loadUseCase: LoadCryptosUseCaseProtocol
+    let subscribeUseCase: SubscribeCryptoUpdatesUseCaseProtocol
     
-    init(loadUseCase: LoadCryptosUseCaseProtocol) {
+    init(loadUseCase: LoadCryptosUseCaseProtocol, subscribeUseCase: SubscribeCryptoUpdatesUseCaseProtocol) {
         self.loadUseCase = loadUseCase
+        self.subscribeUseCase = subscribeUseCase
     }
 }
 
@@ -20,13 +22,28 @@ protocol LoadCryptosUseCaseProtocol {
     func load() async throws -> [CryptoModel]
 }
 
+protocol SubscribeCryptoUpdatesUseCaseProtocol {
+    func subscribe(to cryptos: [String]) -> AsyncThrowingStream<[CryptoModel], Error>
+}
+
 final class CryptosListViewModelTest: XCTestCase {
 
     func test_init_notLoading() {
         let loadUseCase = LoadCryptosUseCaseSpy()
-        let sut = CryptosListViewModel(loadUseCase: loadUseCase)
+        let subscribeUseCase = SubscribeCryptoUpdatesUseCaseSpy()
+        let sut = CryptosListViewModel(loadUseCase: loadUseCase,
+                                       subscribeUseCase: subscribeUseCase)
         
         XCTAssertEqual(loadUseCase.requestCount, 0)
+    }
+    
+    func test_init_notListening() {
+        let loadUseCase = LoadCryptosUseCaseSpy()
+        let subscribeUseCase = SubscribeCryptoUpdatesUseCaseSpy()
+        let sut = CryptosListViewModel(loadUseCase: loadUseCase,
+                                       subscribeUseCase: subscribeUseCase)
+        
+        XCTAssertEqual(subscribeUseCase.subscribeCount, 0)
     }
     
     // MARK: - Helper
@@ -35,6 +52,14 @@ final class CryptosListViewModelTest: XCTestCase {
         
         func load() async throws -> [CryptoModel] {
             [CryptoModel]()
+        }
+    }
+    
+    final class SubscribeCryptoUpdatesUseCaseSpy: SubscribeCryptoUpdatesUseCaseProtocol {
+        let subscribeCount: Int = 0
+        
+        func subscribe(to cryptos: [String]) -> AsyncThrowingStream<[CryptoModel], any Error> {
+            AsyncThrowingStream<[CryptoModel], Error> { _ in }
         }
     }
 }
